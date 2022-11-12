ab626653b46aa7f0cb6c52ad070e6cf82fa1a1b9
alex brito <alex.teixeira.brito@gmail.com>
2022-11-12 16:01:21 -0300

    Projeto criado

---
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/Servlet.java
@@ -0,0 +1,16 @@
+package br.com.gerenciador.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/tela")
+public class Servlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+}
