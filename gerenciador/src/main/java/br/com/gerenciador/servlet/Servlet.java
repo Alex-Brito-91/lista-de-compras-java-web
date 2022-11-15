e5f33dc22ad7385f57f345f8aea0415ea599fb3e
alex brito <alex.teixeira.brito@gmail.com>
2022-11-14 23:11:42 -0300

    Servet configurado

--- gerenciador/src/main/java/br/com/gerenciador/servlet/Servlet.java
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/Servlet.java
@@ -1,16 +1,25 @@
 package br.com.gerenciador.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/tela")
+@WebServlet("/servlet")
 public class Servlet extends HttpServlet {
-	
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("cadastrando item");
 		
+		String nomeItem = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>item " + nomeItem + " cadastrado com sucesso</body></html>");
 	}
 
 }
