9db6fabd6e6862eee0845440bc4343b618d0d52a
alex brito <alex.teixeira.brito@gmail.com>
2023-02-07 07:06:55 -0300

    alterações em classe saldo

--- gerenciador/src/main/java/br/com/gerenciador/servlet/Saldo.java
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/Saldo.java
@@ -3,29 +3,33 @@ package br.com.gerenciador.servlet;
 import java.math.BigDecimal;
 
 public class Saldo {
-	
+
+	private BigDecimal alimentação1;
+	private BigDecimal alimentacao2;
 	private BigDecimal dinheiro;
-	private BigDecimal alimentação;
-	private BigDecimal refeicao;
-	
-	
+
+	public BigDecimal getAlimentação1() {
+		return alimentação1;
+	}
+
+	public void setAlimentação1(BigDecimal alimentação1) {
+		this.alimentação1 = alimentação1;
+	}
+
+	public BigDecimal getAlimentacao2() {
+		return alimentacao2;
+	}
+
+	public void setAlimentacao2(BigDecimal alimentacao2) {
+		this.alimentacao2 = alimentacao2;
+	}
+
 	public BigDecimal getDinheiro() {
 		return dinheiro;
 	}
+
 	public void setDinheiro(BigDecimal dinheiro) {
 		this.dinheiro = dinheiro;
 	}
-	public BigDecimal getAlimentação() {
-		return alimentação;
-	}
-	public void setAlimentação(BigDecimal alimentação) {
-		this.alimentação = alimentação;
-	}
-	public BigDecimal getRefeicao() {
-		return refeicao;
-	}
-	public void setRefeicao(BigDecimal refeicao) {
-		this.refeicao = refeicao;
-	}
-	
+
 }
