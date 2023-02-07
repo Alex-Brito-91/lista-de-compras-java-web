c80936cc65985451aa75f515962b27f851544f40
alex brito <alex.teixeira.brito@gmail.com>
2023-02-07 07:07:11 -0300

    alterações em servlet saldo

--- gerenciador/src/main/java/br/com/gerenciador/servlet/SaldoServlet.java
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/SaldoServlet.java
@@ -1,7 +1,9 @@
 package br.com.gerenciador.servlet;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +15,24 @@ public class SaldoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		BigDecimal valeAlimentacao1 = new BigDecimal(request.getParameter("vale1").replaceAll(",", "."));
+		BigDecimal valeAlimentacao2 = new BigDecimal(request.getParameter("vale2").replaceAll(",", "."));
+		BigDecimal dinheiro = new BigDecimal(request.getParameter("dinheiro").replaceAll(",", "."));
+		
+		Saldo saldo = new Saldo();
+		saldo.setAlimentação1(valeAlimentacao1);
+		saldo.setAlimentacao2(valeAlimentacao2);
+		saldo.setDinheiro(dinheiro);
+		
+		Banco banco = new Banco();
+		banco.adicionaSaldo(saldo);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/formSaldoCriado.jsp");
+		request.setAttribute("vale1", saldo.getAlimentação1());
+		request.setAttribute("vale2", saldo.getAlimentacao2());
+		request.setAttribute("dinheiro", saldo.getDinheiro());
+		rd.forward(request, response);
 		
 	}
 
