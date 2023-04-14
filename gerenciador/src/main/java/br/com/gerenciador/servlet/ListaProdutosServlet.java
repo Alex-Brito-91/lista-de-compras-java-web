641c77a98c8ffef9940a59c47d3a035aac175ffc
alex brito <alex.teixeira.brito@gmail.com>
2023-04-14 11:13:27 -0300

    formatação de moeda retirado do servlet

--- gerenciador/src/main/java/br/com/gerenciador/servlet/ListaProdutosServlet.java
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/ListaProdutosServlet.java
@@ -1,12 +1,10 @@
 package br.com.gerenciador.servlet;
 
 import java.io.IOException;
-
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,29 +14,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/listaProdutos")
 public class ListaProdutosServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("criando lista de produtos");
-		
-		Banco banco = new Banco();
-		Date dataLista = new Date();
-		List<Produto> produtos = banco.getProdutos();
-		
-		String totalF = DecimalFormat.getCurrencyInstance().format(banco.totalCompra());
-		String saldoF = DecimalFormat.getCurrencyInstance().format(banco.saldoTotal());
-		String restanteF = DecimalFormat.getCurrencyInstance().format(banco.saldoRestante());
-		
-		request.setAttribute("data", dataLista);
-		request.setAttribute("produtos", produtos);
-		request.setAttribute("totalCompra", totalF);
-		request.setAttribute("saldoTotal", saldoF);
-		request.setAttribute("totalRestante", restanteF);
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/listaProdutos.jsp");
-		rd.forward(request, response);
-		
-	}
+    private static final long serialVersionUID = 1L;
 
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("criando lista de produtos");
+    	
+    	Banco banco = new Banco();
+    	LocalDate dataLista = LocalDate.now();
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	String dataFormatada = dataLista.format(formatter);
+        List<Produto> produtos = banco.getProdutos();
+        BigDecimal totalCompra = banco.totalCompra();
+        BigDecimal saldoTotal = banco.saldoTotal();
+        BigDecimal saldoRestante = banco.saldoRestante();
+
+        request.setAttribute("data", dataFormatada);
+        request.setAttribute("produtos", produtos);
+        request.setAttribute("totalCompra", totalCompra);
+        request.setAttribute("saldoTotal", saldoTotal);
+        request.setAttribute("saldoRestante", saldoRestante);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/listaProdutos.jsp");
+        rd.forward(request, response);
+    }
 }
