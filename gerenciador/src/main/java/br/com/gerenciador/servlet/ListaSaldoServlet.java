6c59160d0dc9baf4f8e30224aa2a53c484f0b369
alex brito <alex.teixeira.brito@gmail.com>
2023-04-14 11:15:48 -0300

    formatação de moeda retirado do servlet

--- gerenciador/src/main/java/br/com/gerenciador/servlet/ListaSaldoServlet.java
+++ gerenciador/src/main/java/br/com/gerenciador/servlet/ListaSaldoServlet.java
@@ -1,7 +1,7 @@
 package br.com.gerenciador.servlet;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.math.BigDecimal;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,23 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/listaSaldo")
 public class ListaSaldoServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("criando lista de saldo");
-		
-		Banco banco = new Banco();
-		List<Saldo> saldos = banco.getSaldo();
-		
-		String saldoTotal = DecimalFormat.getCurrencyInstance().format(banco.saldoTotal());
-		
-		request.setAttribute("saldos", saldos);
-		request.setAttribute("saldoTotal", saldoTotal);
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/listaSaldos.jsp");
-		rd.forward(request, response);
-		
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+    	System.out.println("criando saldos");
+    	
+        Banco banco = new Banco();
+        List<Saldo> saldos = banco.getSaldo();
 
+        BigDecimal saldoTotal = banco.saldoTotal();
+
+        request.setAttribute("saldos", saldos);
+        request.setAttribute("saldoTotal", saldoTotal);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/listaSaldos.jsp");
+        rd.forward(request, response);
+    }
 }
+
+
+
+
+
